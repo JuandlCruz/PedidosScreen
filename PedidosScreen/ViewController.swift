410e507513cc656d142568d6e38432caac397848
JuandlCruz <juandelacrouz@gmail.com>
2023-03-16 09:57:50 +0100

    Initial Commit

---
+++ PedidosScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PedidosScreen
+//
+//  Created by Juan de la Cruz Sanchez Agudo on 16/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
